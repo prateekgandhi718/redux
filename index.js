824546212b401cc8d4d9d14c74156974cbff8ff0
Prateek Gandhi <Prateek.Gandhi@lntinfotech.com>
2023-02-13 11:47:33 +0530

    single reducer

--- index.js
+++ index.js
@@ -4,24 +4,41 @@ const createStore = redux.createStore
 const CAKE_ORDERED = "CAKE_ORDERED"
 const CAKE_RESTOCKED = "CAKE_RESTOCKED"
 
+const ICECREAM_ORDERED = "ICECREAM_ORDERED"
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED"
 //Action is an object having different properties. Action creator is a function which returns the action (an object)
 
 const orderCake = () => {
     return {
         type: CAKE_ORDERED,
-        quantity: 1
+        payload: 1
     }
 }
 
 const restockCake = (qty) => {
     return {
         type: CAKE_RESTOCKED,
-        quantity: qty
+        payload: qty
+    }
+}
+
+const orderIceCream = () => {
+    return {
+        type: ICECREAM_ORDERED,
+        payload: 1
+    }
+}
+
+const restockIceCream = (qty) => {
+    return {
+        type: ICECREAM_RESTOCKED,
+        payload: qty
     }
 }
 
 const initialState = {
-    numOfCakes: 10
+    numOfCakes: 10,
+    numOfIceCream: 20
 }
 
 //REDUCERS in its simplest form can be defined as (previousState, action) => newState
@@ -31,15 +48,27 @@ const reducer = (state = initialState, action) => {
         case CAKE_ORDERED:
             return {
                 ...state, //this is to copy the initial state object first before making changes. because it can have more than one property.
-                numOfCakes: state.numOfCakes - action.quantity
+                numOfCakes: state.numOfCakes - action.payload
             }
         
         case CAKE_RESTOCKED:
             return {
                 ...state,
-                numOfCakes: state.numOfCakes + action.quantity
+                numOfCakes: state.numOfCakes + action.payload
             }
-    
+            
+        case ICECREAM_ORDERED:
+            return {
+                ...state, //this is to copy the initial state object first before making changes. because it can have more than one property.
+                numOfIceCream: state.numOfIceCream - action.payload
+            }
+        
+        case ICECREAM_RESTOCKED:
+            return {
+                ...state,
+                numOfIceCream: state.numOfIceCream + action.payload
+            }
+
         default:
             return state
     }
@@ -55,6 +84,9 @@ const unsubscribe = store.subscribe(() => {
 store.dispatch(orderCake())
 store.dispatch(orderCake())
 store.dispatch(orderCake())
+store.dispatch(orderIceCream())
+store.dispatch(orderIceCream())
+store.dispatch(restockIceCream(5))
 store.dispatch(restockCake(5))
 
 unsubscribe()
