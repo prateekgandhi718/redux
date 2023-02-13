1c7710c47695ff514082979659d9c2d026cf557c
Prateek Gandhi <Prateek.Gandhi@lntinfotech.com>
2023-02-13 11:09:29 +0530

    redux store

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
 const CAKE_ORDERED = "CAKE_ORDERED"
 
 //Action is an object having different properties. Action creator is a function which returns the action (an object)
@@ -20,10 +23,23 @@ const reducer = (state = initialState, action) => {
         case CAKE_ORDERED:
             return {
                 ...state, //this is to copy the initial state object first before making changes. because it can have more than one property.
-                numOfCakes: state.numOfCakes - 1
+                numOfCakes: state.numOfCakes - action.quantity
             }
     
         default:
             return state
     }
 }
+
+const store = createStore(reducer) // since reducer accepts initial state as an argument, this essentially means our store also has initial state
+console.log('Initial state :', store.getState())
+
+const unsubscribe = store.subscribe(() => {
+    console.log('Updated state :', store.getState())
+})
+
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+unsubscribe()
