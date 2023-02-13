5980aea189a0a2d48afc841e491b2c64ff69e8cb
Prateek Gandhi <Prateek.Gandhi@lntinfotech.com>
2023-02-13 10:34:11 +0530

    actions

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello, Prateek. Nice to see you in office, let's be productive.")
+const CAKE_ORDERED = "CAKE_ORDERED"
+
+//Action is an object having different properties. Action creator is a function which returns the action (an object)
+
+function orderCake () {
+    return {
+        type: CAKE_ORDERED,
+        quantity: 1
+    }
+}
